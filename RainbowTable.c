d84a62f8f7b0de0c1198e5ba29d085ab5ae03338
Unknown <marc.vegetti@etu.esisar.grenoble-inp.fr>
2018-10-18 17:29:27 +0200

    Update1
    Added sha256 code from B-Con
    Added write in File function

--- RainbowTable.c
+++ RainbowTable.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
-#define sizeT 5
+#define sizeT 50000
 
 typedef struct RainbowRow RainbowRow;
 struct RainbowRow {
@@ -25,6 +26,8 @@ char* tailGenerator(char* myHead);
 RainbowTable generateTable(int pL){
 	RainbowTable myTable;
 	myTable.rows = NULL;
+	myTable.passwordLength = pL;
+	myTable.tableSize = sizeT;
 	RainbowRow* nextRow ;
 	int i = 0;
 
@@ -42,23 +45,15 @@ RainbowTable generateTable(int pL){
 	return myTable;
 }
 
-/*"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"*/
-/*char* randomHeadGenerator(int pL){ 
-	int i = 0;
-	char password[pL];
-	printf("-- %c --\n","abcdefghijklmnopqrstuvwxyz0123456789"[random() % 36]);
-	printf("trrr\n");
-	for(i = 0; i < pL-1; i++){
-		strcat(password, "abcdefghijklmnopqrstuvwxyz0123456789"[random() % 36]);
-		printf("eee\n");
-	}
-	return password;
-}*/
+/*"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/" full charset to be set*/
 
-char* tailGenerator(char* myHead){return "oups";}
+char* tailGenerator(char* myHead){
+	return "oups";
+}
 
 static char *randomHeadGenerator(char* str, size_t size){
     const char charset[] = "abcdefghijklmnopqrstuvwxyz0123456789";
+	
     if (size) {	
         for (size_t n = 0; n < size; n++) {
 		int key = rand() % (int) (sizeof charset - 1);
@@ -66,29 +61,50 @@ static char *randomHeadGenerator(char* str, size_t size){
         }
         str[size] = '\0';
     }
+    
     return str;
+}
+
+void createFile(RainbowTable table){
+	FILE *f = fopen("Rainbow1.txt", "w");
+	if (f == NULL)
+	{
+	    printf("Error opening file!\n");
+	    exit(1);
+	}
+
+	/* print table info */
+	fprintf(f, "### Rainbow table information ###\n");
+	fprintf(f, "Table size: %d\n", table.tableSize);
+	fprintf(f, "Password length: %d\n\n", table.passwordLength);
+	fprintf(f, "### Rainbow table datas ###\n");
+
+	/* print table data */
+	RainbowRow* current = table.rows;
+	while(current != NULL){
+		fprintf(f,"%s %s\n",current->head,current->tail);
+		current = current->next;
+	}
+	fclose(f);
 }
 
 
 
 int main(int argc, char* argv[]){
+	clock_t begin0 = clock();
 	printf("Start Program ...\n");
 	printf("Creating table of size : %d ...\n",sizeT);
-	RainbowTable table = generateTable(4);
+	RainbowTable table = generateTable(8);
+	clock_t end = clock();
+	clock_t begin1 = clock();
+	double time_spent = (double)(end - begin0) / CLOCKS_PER_SEC;
+	printf("Table creation time: %d\n",time_spent);
 	printf("Creation completed ...\n");
-	RainbowRow* current = table.rows;
-	while(current != NULL){
-		printf("%s %s\n",current->head,current->tail);
-		current = current->next;
-	}
+	printf("File creation ...\n");
+	createFile(table);
+	end = clock();
+	time_spent = (double)(end - begin1) / CLOCKS_PER_SEC;
+	printf("File creation time: %d\n",time_spent);
+	time_spent = (double)(end - begin0) / CLOCKS_PER_SEC;
+	printf("Total execution time: %d\n",time_spent);
 }
-
-/*	public String randomHeadGenerator(int passwordLength) {
-		StringBuilder result = new StringBuilder();
-		String set = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-		for (int i = 0; i < passwordLength; i++) {
-			int characterIndex = new SecureRandom().nextInt(set.length());
-			result.append(set.charAt(characterIndex));
-		}
-		return result.toString();
-	}*/
---
+++ sha256.h
@@ -0,0 +1,26 @@
+#ifndef SHA256_H
+#define SHA256_H
+
+/*************************** HEADER FILES ***************************/
+#include <stddef.h>
+
+/****************************** MACROS ******************************/
+#define SHA256_BLOCK_SIZE 32            
+
+/**************************** DATA TYPES ****************************/
+typedef unsigned char BYTE;             
+typedef unsigned int  WORD;             
+
+typedef struct {
+	BYTE data[64];
+	WORD datalen;
+	unsigned long long bitlen;
+	WORD state[8];
+} SHA256_CTX;
+
+/*********************** FUNCTION DECLARATIONS **********************/
+void sha256_init(SHA256_CTX *ctx);
+void sha256_update(SHA256_CTX *ctx, const BYTE data[], size_t len);
+void sha256_final(SHA256_CTX *ctx, BYTE hash[]);
+
+#endif   
